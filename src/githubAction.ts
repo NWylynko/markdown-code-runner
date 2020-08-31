363971929e82db87bc0ac068775e54651a3f321c
Nick Wylynko <nick1014375@gmail.com>
2020-08-31 18:24:43 +0800

    need to actually run it

--- src/githubAction.ts
+++ src/githubAction.ts
@@ -2,19 +2,15 @@
 import * as github from "@actions/github";
 import run from "./index"
 
-const runAction = () => {
-  // get the name of the repo this action is running in
-  const fullRepo = github.context.payload.repository.full_name
+// get the name of the repo this action is running in
+const fullRepo = github.context.payload.repository.full_name
 
-  const repo = fullRepo.split("/")[1];
+const repo = fullRepo.split("/")[1];
 
-  // only want to run the code in the repo this is being run on
-  const repoDir = `/home/runner/work/${repo}/${repo}`;
+// only want to run the code in the repo this is being run on
+const repoDir = `/home/runner/work/${repo}/${repo}`;
 
-  // all the markdown files in the repoDir
-  const folders = `${repoDir}/**/*.md`;
+// all the markdown files in the repoDir
+const folders = `${repoDir}/**/*.md`;
 
-  run(folders)
-}
-
-export default runAction
+run(folders)
