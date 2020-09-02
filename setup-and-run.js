60a531a36735501faf31d051511eb7fb842874f3
Nick Wylynko <nick1014375@gmail.com>
2020-09-02 09:41:57 +0800

    Update setup-and-run.js

--- setup-and-run.js
+++ setup-and-run.js
@@ -1,6 +1,6 @@
 const { spawn } = require("child_process")
 
-const test = spawn("tree", ["/home/runner/"])
+const test = spawn("ls", ["/home/runner/"])
 
 test.stdout.on("data", data => console.log(data.toString()))
 
