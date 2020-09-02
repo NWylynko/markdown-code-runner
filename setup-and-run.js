b02fbface73258432b57728a4883a8f42a706b92
Nick Wylynko <nick1014375@gmail.com>
2020-09-02 10:00:56 +0800

    Update setup-and-run.js

--- setup-and-run.js
+++ setup-and-run.js
@@ -1,61 +1,41 @@
 const { spawn } = require("child_process")
 
-const base = "/home/runner/work/"
+const cwd = "/home/runner/work/_actions/nwylynko/markdown-code-runner/master"
 
-const test = spawn("ls", [base])
-const test2 = spawn("ls", [base + "_actions"])
-const test3 = spawn("ls", [base + "_actions/actions"])
-const test4 = spawn("ls", [base + "_actions/nwylynko"])
-const test5 = spawn("ls", [base + "_actions/nwylynko/markdown-code-runner"])
-const test6 = spawn("ls", [base + "_actions/nwylynko/markdown-code-runner/master"])
-const test7 = spawn("ls", [base + "_actions/nwylynko/markdown-code-runner/master.completed"])
-const test8 = spawn("ls", [base + "_actions/nwylynko/markdown-code-runner/*/*"])
+const installDependenciesRunner = spawn("yarn", [], { cwd })
 
-test.stdout.on("data", data => console.log(data.toString()))
-test2.stdout.on("data", data => console.log(data.toString()))
-test3.stdout.on("data", data => console.log(data.toString()))
-test4.stdout.on("data", data => console.log(data.toString()))
-test5.stdout.on("data", data => console.log(data.toString()))
-test6.stdout.on("data", data => console.log(data.toString()))
-test7.stdout.on("data", data => console.log(data.toString()))
-test8.stdout.on("data", data => console.log(data.toString()))
+installDependenciesRunner.stdout.on("data", (data) => {
+  data = data.toString()
+  console.log(data)
+})
 
-// const cwd = "/home/runner/work/markdown-code-runner/markdown-code-runner/"
+installDependenciesRunner.stderr.on("data", (data) => {
+  data = data.toString()
+  console.log(data)
+})
 
-// const installDependenciesRunner = spawn("yarn", [], { cwd })
+installDependenciesRunner.on("close", async (code) => {
+  if (code === 0 || code === null ) {
+    console.log("installed dependencies")
+    const exitCode = await runAction()
+    process.exit(exitCode)
+  } else {
+    console.log("failed to install dependencies")
+    process.exit(code)
+  }
+})
 
-// installDependenciesRunner.stdout.on("data", (data) => {
-//   data = data.toString()
-//   console.log(data)
-// })
-
-// installDependenciesRunner.stderr.on("data", (data) => {
-//   data = data.toString()
-//   console.log(data)
-// })
-
-// installDependenciesRunner.on("close", async (code) => {
-//   if (code === 0 || code === null ) {
-//     console.log("installed dependencies")
-//     const exitCode = await runAction()
-//     process.exit(exitCode)
-//   } else {
-//     console.log("failed to install dependencies")
-//     process.exit(code)
-//   }
-// })
-
-// const runAction = () => {
-//   return new Promise((resolve, reject) => {
-//     const runner = spawn("node", ["./dist/githubAction.js"], { cwd })
-//     runner.stdout.on("data", data => process.stdout.write(data))
-//     runner.stderr.on("data", data => process.stderr.write(data))
-//     runner.on("close", code => { 
-//       if (code === 0 || code === null) {
-//         resolve(code)
-//       } else {
-//         reject(code)
-//       } 
-//     })
-//   })
-// }
+const runAction = () => {
+  return new Promise((resolve, reject) => {
+    const runner = spawn("node", ["./dist/githubAction.js"], { cwd })
+    runner.stdout.on("data", data => process.stdout.write(data))
+    runner.stderr.on("data", data => process.stderr.write(data))
+    runner.on("close", code => { 
+      if (code === 0 || code === null) {
+        resolve(code)
+      } else {
+        reject(code)
+      } 
+    })
+  })
+}
