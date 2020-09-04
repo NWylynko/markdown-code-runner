e4e55c4fec119320ba3674d11273959c963a06c9
distribution builder <nick1014375@gmail.com>
2020-09-04 14:59:03 +0000

    Updated distribution

--- dist/dev.js
+++ dist/dev.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("./index"));
 // all the markdown files in the repoDir
-const folders = `${__dirname}/../examples/*.md`;
+const folders = `${__dirname}/../**/*.md`;
 index_1.default(folders);
